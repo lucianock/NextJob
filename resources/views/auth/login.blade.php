a370618a77cb37f5e5a7c8f59724696e2519846d
Luciano <62312023+lucianock@users.noreply.github.com>
2025-05-06 19:55:20 -0300

    test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-page-heading>Log In</x-page-heading>
 
-    <x-forms.form method="POST" action="/login">
+    <x-forms.form method="POST" :action="url('/login')">
         <x-forms.input label="Email" name="email" type="email" />
         <x-forms.input label="Password" name="password" type="password" />
 
