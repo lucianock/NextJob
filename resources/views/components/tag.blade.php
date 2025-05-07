0aee7a07f9ede3febbeee340dbb950e171bf66e3
Luciano <62312023+lucianock@users.noreply.github.com>
2025-05-07 18:24:41 -0300

    fixe route name tag

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,15 +1,20 @@
 @props(['tag', 'size' => 'base'])
 
 @php
-    $classes = "bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300";
+    $classes = 'bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300';
 
     if ($size === 'base') {
-        $classes .= " px-5 py-1 text-sm";
+        $classes .= ' px-5 py-1 text-sm';
     }
 
     if ($size === 'small') {
-        $classes .= " px-3 py-1 text-2xs";
+        $classes .= ' px-3 py-1 text-2xs';
     }
 @endphp
 
-<a href="/tags/{{ strtolower($tag->name) }}" class="{{ $classes }}">{{ ucwords($tag->name) }}</a>
+<a 
+    href="{{ route('tags.show', $tag->name) }}" 
+    class="{{ $classes }}"
+>
+    {{ ucwords($tag->name) }}
+</a>
